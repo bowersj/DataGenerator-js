8c40d7966d78b7280cd1fab103cbda043313cfb4
Josh Bowers <josh.bowers.usa@gmail.com>
2019-09-02 23:55:21 -0600

    started drivers license generation

---
+++ src/dataGeneration/driversLicense/US.js
@@ -0,0 +1,6 @@
+// how to format for different states
+// https://ntsi.com/drivers-license-format/
+//
+// International Drivers License
+// Explanation of UK driver license format
+// https://stackoverflow.com/questions/45493570/validate-uk-driving-license-regex-in-javascript#answer-46447128
